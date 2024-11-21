1f5373028c45c0c934c6e9cf165c8163f2d695e3
devops-geomais-julio <devops.geomais@gmail.com>
2024-11-21 10:46:19 -0300

    ADD error handling

--- handlers/scrapper.go
+++ handlers/scrapper.go
@@ -1,53 +1,87 @@
 package handlers
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"strings"
-     "fmt"
-	"github.com/gocolly/colly"
+  "fmt"
+  "github.com/gofiber/fiber/v2"
+  "github.com/gocolly/colly"
+  "strings"
 )
 
+// Global variable to store the scraped version (optional for caching)
+var cachedVersion string = ""
+
+// ScrapeVersion fetches the latest version from the website
+func ScrapeVersion() (string, error) {
+  if cachedVersion != "" {
+    return cachedVersion, nil
+  }
+
+  var arrVersao []string
+  c := colly.NewCollector(
+    colly.AllowedDomains("sisaps.saude.gov.br"),
+  )
+
+  c.OnHTML(".pricing-table li h3", func(e *colly.HTMLElement) {
+    versaoElement := e.Text
+    arr := strings.Split(versaoElement, " ")
+
+    // Check if splitting resulted in at least 2 elements
+    if len(arr) < 2 {
+      fmt.Println("Warning: Version element format unexpected")
+      return
+    }
+
+    arrVersao = append(arrVersao, arr[1])
+  })
+
+  err := c.Visit("https://sisaps.saude.gov.br/esus/")
+  if err != nil {
+    return "", fmt.Errorf("Error visiting website: %w", err)
+  }
+
+  if len(arrVersao) == 0 {
+    return "", fmt.Errorf("No version found on the page")
+  }
+
+  cachedVersion = arrVersao[0]
+  return cachedVersion, nil
+}
+
+// Home handler displays available endpoints
 func Home(c *fiber.Ctx) error {
-		return c.SendString("/versaoesus ou /verificaversao?v=5.x.xx")
+  return c.SendString("Available endpoints: /versaoesus, /verificaversao?v=X.X.X")
 }
 
+// VersaoEsus handler fetches and returns the current version
 func VersaoEsus(c *fiber.Ctx) error {
-	return c.SendString(scrapVersao())
+  version, err := ScrapeVersion()
+  if err != nil {
+    return c.Status(fiber.StatusInternalServerError).SendString(fmt.Sprintf("Error fetching version: %v", err))
+  }
+  return c.SendString(version)
 }
 
+// VerificaVersao handler compares provided version with the scraped version
 func VerificaVersao(c *fiber.Ctx) error {
-	var versao string = c.Query("v")
-	fmt.Println(versao)
-	return c.SendString(comparaVersao(versao))
-}
+  requestedVersion := c.Query("v")
+  if requestedVersion == "" {
+    return c.Status(fiber.StatusBadRequest).SendString("-1")
+  }
 
-func scrapVersao() string {
-	var arrVersao []string
-	// instantiate a new collector object
-	c := colly.NewCollector(
-		colly.AllowedDomains("sisaps.saude.gov.br"),
-	)
-	c.OnHTML(".pricing-table li h3", func(e *colly.HTMLElement) {
-		var versaoElement = e.Text
-		arr := strings.Split(versaoElement, " ")
-		arrVersao = append(arrVersao, arr[1])
-	})
-	// open the target URL
-	c.Visit("https://sisaps.saude.gov.br/esus/")
-	return arrVersao[0]
-}
+  currentVersion, err := ScrapeVersion()
+  if err != nil {
+    return c.Status(fiber.StatusInternalServerError).SendString(fmt.Sprintf("Error fetching version: %v", err))
+  }
 
-func comparaVersao(versao string) string {
-	var versaoAtual string
-	versaoAtual = scrapVersao()
-	var retorno = "-1"
-	if (len(versao) > 0){
-		if(versaoAtual != versao){
-			retorno = "0"
-		}
-		if(versaoAtual == versao){
-			retorno = "1"
-		}
-	}
-	return retorno
+  var comparisonResult string
+  switch {
+  case requestedVersion < currentVersion:
+    comparisonResult = "0" // Update required
+  case requestedVersion == currentVersion:
+    comparisonResult = "1" // Versions match
+  default:
+    comparisonResult = "0" // Downgrade not recommended
+  }
+
+  return c.SendString(comparisonResult)
 }
