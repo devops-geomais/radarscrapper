d4b1eb9b96fc8c72568081b6691ea30c75be81c1
devops-geomais-julio <devops.geomais@gmail.com>
2025-02-03 09:38:13 -0300

    fix bypass certificate

--- handlers/scrapper.go
+++ handlers/scrapper.go
@@ -1,10 +1,13 @@
 package handlers
 
 import (
-  "fmt"
-  "github.com/gofiber/fiber/v2"
-  "github.com/gocolly/colly"
-  "strings"
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gocolly/colly"
 )
 
 // Global variable to store the scraped version (optional for caching)
@@ -12,76 +15,83 @@ var cachedVersion string = ""
 
 // ScrapeVersion fetches the latest version from the website
 func ScrapeVersion() (string, error) {
-  if cachedVersion != "" {
-    return cachedVersion, nil
-  }
+	if cachedVersion != "" {
+		return cachedVersion, nil
+	}
 
-  var arrVersao []string
-  c := colly.NewCollector(
-    colly.AllowedDomains("sisaps.saude.gov.br"),
-  )
+	var arrVersao []string
 
-  c.OnHTML(".pricing-table li h3", func(e *colly.HTMLElement) {
-    versaoElement := e.Text
-    arr := strings.Split(versaoElement, " ")
+	// Criando um coletor normal
+	c := colly.NewCollector(
+		colly.AllowedDomains("sisaps.saude.gov.br"),
+	)
 
-    // Check if splitting resulted in at least 2 elements
-    if len(arr) < 2 {
-      fmt.Println("Warning: Version element format unexpected")
-      return
-    }
+	// Modificando diretamente o Transport do coletor
+	c.WithTransport(&http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, // Ignora SSL
+	})
 
-    arrVersao = append(arrVersao, arr[1])
-  })
+	c.OnHTML(".pricing-table li h3", func(e *colly.HTMLElement) {
+		versaoElement := e.Text
+		arr := strings.Split(versaoElement, " ")
 
-  err := c.Visit("https://sisaps.saude.gov.br/esus/")
-  if err != nil {
-    return "", fmt.Errorf("Error visiting website: %w", err)
-  }
+		// Check if splitting resulted in at least 2 elements
+		if len(arr) < 2 {
+			fmt.Println("Warning: Version element format unexpected")
+			return
+		}
 
-  if len(arrVersao) == 0 {
-    return "", fmt.Errorf("No version found on the page")
-  }
+		arrVersao = append(arrVersao, arr[1])
+	})
 
-  cachedVersion = arrVersao[0]
-  return cachedVersion, nil
+	err := c.Visit("https://sisaps.saude.gov.br/esus/")
+	if err != nil {
+		return "", fmt.Errorf("Error visiting website: %w", err)
+	}
+
+	if len(arrVersao) == 0 {
+		return "", fmt.Errorf("No version found on the page")
+	}
+
+	cachedVersion = arrVersao[0]
+	return cachedVersion, nil
 }
 
 // Home handler displays available endpoints
 func Home(c *fiber.Ctx) error {
-  return c.SendString("Available endpoints: /versaoesus, /verificaversao?v=X.X.X")
+	return c.SendString("Available endpoints: /versaoesus, /verificaversao?v=X.X.X")
 }
 
 // VersaoEsus handler fetches and returns the current version
 func VersaoEsus(c *fiber.Ctx) error {
-  version, err := ScrapeVersion()
-  if err != nil {
-    return c.Status(fiber.StatusInternalServerError).SendString(fmt.Sprintf("Error fetching version: %v", err))
-  }
-  return c.SendString(version)
+	version, err := ScrapeVersion()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(fmt.Sprintf("Error fetching version: %v", err))
+	}
+	return c.SendString(version)
 }
 
 // VerificaVersao handler compares provided version with the scraped version
 func VerificaVersao(c *fiber.Ctx) error {
-  requestedVersion := c.Query("v")
-  if requestedVersion == "" {
-    return c.Status(fiber.StatusBadRequest).SendString("-1")
-  }
+	requestedVersion := c.Query("v")
+	if requestedVersion == "" {
+		return c.Status(fiber.StatusBadRequest).SendString("-1")
+	}
 
-  currentVersion, err := ScrapeVersion()
-  if err != nil {
-    return c.Status(fiber.StatusInternalServerError).SendString(fmt.Sprintf("Error fetching version: %v", err))
-  }
+	currentVersion, err := ScrapeVersion()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(fmt.Sprintf("Error fetching version: %v", err))
+	}
 
-  var comparisonResult string
-  switch {
-  case requestedVersion < currentVersion:
-    comparisonResult = "0" // Update required
-  case requestedVersion == currentVersion:
-    comparisonResult = "1" // Versions match
-  default:
-    comparisonResult = "0" // Downgrade not recommended
-  }
+	var comparisonResult string
+	switch {
+	case requestedVersion < currentVersion:
+		comparisonResult = "0" // Update required
+	case requestedVersion == currentVersion:
+		comparisonResult = "1" // Versions match
+	default:
+		comparisonResult = "0" // Downgrade not recommended
+	}
 
-  return c.SendString(comparisonResult)
+	return c.SendString(comparisonResult)
 }
